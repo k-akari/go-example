679fde7d945a0ccbebeca5fbf94e3ab545ea88c8
k-akari <tennis.akari.abcdefg@gmail.com>
2022-04-29 10:17:56 +0900

    buildディレクトリへ移動させる

---
+++ build/database/insert_seed_data.sql
@@ -0,0 +1,3 @@
+delete from users;
+insert into users values(1, 'uuid001', 'name1', 'name1@gmail.com', 'password1');
+insert into users values(2, 'uuid002', 'name2', 'name2@gmail.com', 'password2');
