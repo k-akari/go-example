96ff8fdb03fbecccbd83302495a60529525e8d42
k-akari <tennis.akari.abcdefg@gmail.com>
2022-04-29 10:18:18 +0900

    buildディレクトリへ移動させる

--- web/database/create_tables.sql
+++
@@ -1,4 +0,0 @@
-create table users (
-  id bigint primary key,
-  password varchar(20) not null
-);
--- web/database/insert_seed_data.sql
+++
@@ -1,3 +0,0 @@
-delete from users;
-insert into users values(1, 'password1');
-insert into users values(2, 'password2');
